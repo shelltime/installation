d3bae5b181e1d6427412273fc89dd53238d1da3e
AnnatarHe <annatar.he+github@gmail.com>
2024-10-12 22:21:40 +0800

    fix(script): disable background saving to avoid race problem

--- hooks/zsh.zsh
+++ hooks/zsh.zsh
@@ -5,10 +5,10 @@ MALAM_SESSION_ID=$(date +%Y%m%d%H%M%S)
 
 # Define the preexec function
 preexec() {
-    malamtime track -s=zsh -id=$MALAM_SESSION_ID -cmd=$argv -p=pre &
+    malamtime track -s=zsh -id=$MALAM_SESSION_ID -cmd=$argv -p=pre
 }
 
 # Define the postexec function (in zsh, it's called precmd)
 precmd() {
-    malamtime track -s=zsh -id=$MALAM_SESSION_ID -cmd=$argv -p=post -r=$? &
+    malamtime track -s=zsh -id=$MALAM_SESSION_ID -cmd=$argv -p=post -r=$?
 }
